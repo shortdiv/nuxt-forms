45ffca109def6a0afd3532c3199ff308941b2712
Divya Sasidharan <hello@shortdiv.com>
2019-06-26 17:24:20 -0400

    adding field name

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -5,20 +5,24 @@
         <input type="hidden" name="contact" value="contact-form">
         <v-text-field
           v-model="name"
+          name="name"
           label="Your Name"
           type="text"
         />
         <v-text-field
           v-model="email"
+          name="email"
           label="Your Email"
           type="email"
         />
         <v-select
           :items="items"
+          name="role"
           label="Your Role"
         />
         <v-text-field
           v-model="message"
+          name="message"
           label="message"
         />
         <v-btn type="submit">
