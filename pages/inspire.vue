050251c3d30690d2f504fcc19b1636ec036857e0
Divya Sasidharan <hello@shortdiv.com>
2019-06-26 16:40:50 -0400

    trying

---
+++ pages/inspire.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <v-form ref="form" name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="contact" value="contact-form">
+        <v-text-field
+          v-model="name"
+          label="Your Name"
+          type="text"
+        />
+        <v-text-field
+          v-model="email"
+          label="Your Email"
+          type="email"
+        />
+        <v-select
+          :items="items"
+          label="Your Role"
+        />
+        <v-text-field
+          v-model="message"
+          label="message"
+        />
+        <v-btn type="submit">
+          submit
+        </v-btn>
+      </v-form>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: null,
+      email: null,
+      message: null,
+      items: ['Leader', 'Follower']
+    }
+  }
+}
+</script>
